118e3be6a92805625007106ed03212fb5b034054
Vo Dang Chuong <chuong.vd@neo-lab.vn>
2018-11-12 23:21:06 +0700

    login

---
+++ resources/lang/en/message.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'login' => [
+		'error' => [
+			'failed' => 'Login failed'
+		],
+		'success' => 'Login successfuly'
+	],
+	'register' => [
+		'error' => [
+			'failed' => 'Register failed'
+		],
+		'success' => 'Register successfuly'
+	],
+	'server_error' => 'Server error'
+];
